d7432aa98eed2375be7dcd8813f77d71c985efc7
Son Vo <duyson1999@gmail.com>
2025-06-24 22:21:13 +0700

    basic js

---
+++ java-script/index.js
@@ -0,0 +1,46 @@
+var number = 5;
+console.log(number)
+
+// Data Types:
+// underfined, null, boolean, string, symbol, number, and object
+
+
+// 3 ways to declare variables: var, let, const
+// var is reusable
+var myName = "SonVo"
+myName = 8
+
+let outName = "freeCodeCamp"
+
+// const is never changed, ONLY one const name
+const pi = 3.14
+// const pi = 567 This will raise error
+
+
+var a;
+var b = 2;
+console.log(a)
+
+a = 7
+
+b = a;
+
+console.log(a)
+
+
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " string!";
+console.log(a, b, c)
+
+
+
+var firstNameLength = 0;
+var firstName = "Ada";
+firstNameLength = firstName.length;
+console.log('firstNameLength:', firstNameLength)
