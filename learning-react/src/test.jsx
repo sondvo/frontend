115b0db436cc555d16b840345766364950459143
Son Vo <duyson1999@gmail.com>
2025-07-07 23:52:03 +0700

    start react

--- learning-react/src/test.jsx
+++ learning-react/src/test.jsx
@@ -1,10 +1,28 @@
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById('root'))
+
+// React treats lowercase tag names as HTML tags (like <div>, <span>, <main>)
+// React treats Capitalized names as custom components (like <MainContent />)
+
+function MainContent() {
+	return <h1>React is great!</h1>
+}
+
+	// This might work. But it's not the React way!!!
+	// Use div instead
+// root.render(
+// 	MainContent()
+// )
+
 root.render(
-	<ul>
-		<li>sc-toolkits</li>
-		<li>interactive apps</li>
-		<li>independent</li>
-	</ul>
+	<div>
+		{/* call the function */}
+		<MainContent />
+	</div>
+)
+
+// or
+root.render(
+	<MainContent />
 )
